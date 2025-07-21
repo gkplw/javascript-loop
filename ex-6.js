3c10fbdfe45123466e2e1af30d0a966e8e72065b
Pharnuwat <se56160411@gmail.com>
2025-07-21 14:07:09 +0700

    edit: update string reversal logic to use for...of syntax

--- ex-6.js
+++ ex-6.js
@@ -3,13 +3,17 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 //Start coding here
-for(let i = 0;i < companyName.length;i++){
-    reversedCompanyName = companyName[i] + reversedCompanyName;
-}
-console.log(reversedCompanyName);
+// for(let i = 0;i < companyName.length;i++){
+//     reversedCompanyName = companyName[i] + reversedCompanyName;
+// }
+// console.log(reversedCompanyName);
 
 // for(let i = companyName.length - 1; i >= 0; i--){
 //     reversedCompanyName += companyName[i];
 // }
 // console.log(reversedCompanyName);
 
+for(let i of companyName){
+    reversedCompanyName = i + reversedCompanyName;
+}
+console.log(reversedCompanyName);
